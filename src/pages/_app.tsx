cb611162d4ec657cd4520c10740651e0e745ab32
Seunghyun Min <i@seunghyun.in>
2023-04-14 00:53:14 +0900

    feat: create theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
-import "@/styles/globals.css";
-
 import type { AppProps } from "next/app";
+import { ThemeProvider } from "styled-components";
+
+import { defaultTheme } from "@/styles/theme";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Component {...pageProps} />;
+    </ThemeProvider>
+  );
 }
