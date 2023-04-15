ac0b00c0d995dcbd072c8b520de222eb1e3cf6a7
Seunghyun Min <i@seunghyun.in>
2023-04-15 19:08:24 +0900

    chore: add react-icons

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { IconContext } from "react-icons";
 import { ThemeProvider } from "styled-components";
 
 import GlobalStyles from "@/styles/GlobalStyles";
@@ -11,10 +12,12 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <ThemeProvider theme={defaultTheme}>
-        <GlobalStyles />
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <IconContext.Provider value={{}}>
+        <ThemeProvider theme={defaultTheme}>
+          <GlobalStyles />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </IconContext.Provider>
     </>
   );
 }
