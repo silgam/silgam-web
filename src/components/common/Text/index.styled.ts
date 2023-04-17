b685f406593e42d9db997018f609098bcb237d36
Seunghyun Min <i@seunghyun.in>
2023-04-18 01:29:05 +0900

    feat: adjust font size of sperscript responsively (close #9)

--- src/components/common/Text/index.styled.ts
+++ src/components/common/Text/index.styled.ts
@@ -7,4 +7,12 @@ export const Bold = styled.span`
 export const Superscript = styled.sup`
   font-size: 9px;
   vertical-align: text-top;
+
+  @media (max-width: 830px) {
+    font-size: 7px;
+  }
+
+  @media (max-width: 480px) {
+    font-size: 5px;
+  }
 `;
